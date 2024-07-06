1283872443f4a204b20bdd962dc6050710a6b606
siripireddy-chandu <chandusiripireddy2001@gmail.com>
2024-07-06 17:18:31 +0530

    Update GoogleScript.js

--- GoogleScript.js
+++ GoogleScript.js
@@ -11,7 +11,7 @@
 //         .catch(error => console.error('Error!', error.message))
 // })
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzrLJtxcchXVpXdnUPaD64ct7IYMi-CZIiU2fFEiR90ZMTsKp4-I5Q1IQeIgNUFDyzC/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwHIBO595mHJSLN0pgry36z2nzSEpsZnFA4eNeMUoqdUMk0blaTNlMxGx6qqc_PzlHu/exec';
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.forms['contact-form'];
